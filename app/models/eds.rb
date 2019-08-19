582c90f1f8a6545d3941211c5f9dc2816209d5a2
Nestor Walker <naw4t@virginia.edu>
2019-08-19 13:03:50 -0400

    Revert "remove synchronize for now"
    This reverts commit 2ef4641f0f5e77ac6f6bfc9d2ac48a5ce73822c1.

--- app/models/eds.rb
+++ app/models/eds.rb
@@ -5,22 +5,25 @@ class EDS
   base_uri ENV['EDS_BASE_URI']
   format :json
 
+  @@lock = Mutex.new
   def login
-    $logger.debug 'Logging in'
-    auth = self.class.post('/authservice/rest/UIDAuth',
-                           body: {'UserId' => ENV['EDS_USER'], 'Password' => ENV['EDS_PASS']}.to_json,
-                           headers: base_headers
-                          )
+    @@lock.synchronize do
+      $logger.debug 'Logging in'
+      auth = self.class.post('/authservice/rest/UIDAuth',
+                             body: {'UserId' => ENV['EDS_USER'], 'Password' => ENV['EDS_PASS']}.to_json,
+                             headers: base_headers
+                            )
 
-    session = self.class.post('/edsapi/rest/CreateSession',
-                              body: {'Profile' => ENV['EDS_PROFILE'],
-                                     'Guest' => 'n',
-                                     'Org' => ENV['EDS_ORG']}.to_json,
-    headers: base_headers.merge({'x-authenticationToken' => auth['AuthToken']})
-                             )
-    @@auth_token = auth['AuthToken']
-    @@auth_timeout = Time.now + auth['AuthTimeout'].to_i
-    @@session_token = session['SessionToken']
+      session = self.class.post('/edsapi/rest/CreateSession',
+                                body: {'Profile' => ENV['EDS_PROFILE'],
+                                       'Guest' => 'n',
+                                       'Org' => ENV['EDS_ORG']}.to_json,
+      headers: base_headers.merge({'x-authenticationToken' => auth['AuthToken']})
+                               )
+      @@auth_token = auth['AuthToken']
+      @@auth_timeout = Time.now + auth['AuthTimeout'].to_i
+      @@session_token = session['SessionToken']
+    end
   end
 
   def base_headers
