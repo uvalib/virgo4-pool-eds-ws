f7b0ae97281f1a118d2bfb6f2ee510d838834cde
Nestor Walker <naw4t@virginia.edu>
2019-08-20 15:17:40 -0400

    fixed available facets

--- test.rb
+++ test.rb
@@ -2,21 +2,18 @@ require 'rubygems'
 require 'cuba/test'
 require 'bundler'
 require 'logger'
+require 'pry-debugger-jruby'
+require 'dotenv/load'
 Bundler.setup
 
 $logger = Logger.new($stdout)
-$logger.level = Logger::INFO
-if ENV['RACK_ENV'] != 'production'
-  require 'pry-debugger-jruby'
-  require 'dotenv/load'
-  $logger.level = Logger::DEBUG
-end
+$logger.level = Logger::DEBUG
 
 Dir[File.join(__dir__, 'app', '**', '*.rb')].each { |file| require file }
 
 scope do
   test 'Search' do
-    post '/api/search', {query: 'title:{bananas}'}
+    post '/api/search', {query: 'title:{google}'}
     puts last_response.body
     assert last_response.ok?
   end
