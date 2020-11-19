9d373900c877a468d97e348ec25b61ac2c71ad0c
Nestor Walker <naw4t@virginia.edu>
2020-11-19 17:38:32 -0500

    Fixed default headers/CORS issue

--- config/cuba.rb
+++ config/cuba.rb
@@ -5,5 +5,5 @@ Cuba.plugin(Cuba::Render)
 
 Cuba.settings[:render][:template_engine] = "jbuilder"
 Cuba.settings[:render][:views] = "app/views/"
-Cuba.settings[:default_headers] = {'Content-Type' => 'application/json; charset=utf-8'}
+Cuba.settings[:default_headers].merge!({'Content-Type' => 'application/json; charset=utf-8'})
 
