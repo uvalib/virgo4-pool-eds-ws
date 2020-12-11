c92a7efc443dbcaa22471e25f99e5feb4ed12262
Jason Jordan <jlj5aj@virginia.edu>
2020-12-10 20:22:21 -0500

    update facet list for a post-pool-merge world

--- app/models/identify.rb
+++ app/models/identify.rb
@@ -1,11 +1,9 @@
 class Identify
   ATTRIBUTES = [
-    {name: 'logo_url', supported: false },
     {name: 'external_url', supported: false},
-    {name: 'uva_ils', supported: false},
     {name: 'facets', supported: true},
-    {name: 'cover_images', supported: false},
-    {name: "course_reserves", supported: false},
-    {name: "sorting", supported: true}
+    {name: 'item_message', supported: false},
+    {name: 'logo_url', supported: false},
+    {name: 'sorting', supported: true}
   ]
 end
